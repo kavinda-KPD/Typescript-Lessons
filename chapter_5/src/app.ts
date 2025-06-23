5c8fd4c5ecdccafdc81b76c1d4a56c1ebb0a5aa5
kavinda-KPD <kavindadissanayake1999@gmail.com>
2025-06-23 16:27:40 +0530

    singleton pattern with the static method

--- chapter_5/src/app.ts
+++ chapter_5/src/app.ts
@@ -1,40 +1,29 @@
-// Code goes here!
+class Person {
+  name: string;
+  age: number;
 
-class Department {
-  // constructor(private readonly name: string, private employees: string[]) {}
+  private static person: Person;
 
-  name: string = "Default";
-  employees: string[] = ["Default"];
-  // constructor() {
-  //   this.name = "Default";
-  //   this.employees = ["default emp"];
-  // }
-
-  describe(this: Department) {
-    console.log("Department from class: " + this.name);
-  }
-
-  addEmployee(employee: string) {
-    this.employees.push(employee);
-  }
-
-  printEmployeeInformation() {
-    console.log(this.employees);
-  }
-}
-
-class ITDepartment extends Department {
-  constructor(name: string, employees: string[], public admins: string[]) {
-    // super(name, employees);
-    super();  
+  private constructor(name: string, age: number) {
     this.name = name;
-    this.employees = employees;
-    this.admins = admins;
+    this.age = age;
+  }
+
+  static createPerson(name: string, age: number) {
+    if (Person.person) {
+      return Person.person;
+    }
+
+    Person.person = new Person(name, age);
+    return Person.person;
   }
 }
 
-const IT1 = new ITDepartment("kavinda", ["em1", "em2"], ["admin1", "admin2"]);
+// const person = new Person("John", 30);
+// console.log(person);
 
-console.log(IT1);
-IT1.addEmployee("em3");
-IT1.printEmployeeInformation();
+const person = Person.createPerson("John", 30);
+console.log(person);
+
+const person2 = Person.createPerson("John", 31);
+console.log(person2);
