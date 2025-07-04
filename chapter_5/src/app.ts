f2e0c0ef3b40426f2b30e7465ac9791efa7348b4
kavinda-KPD <kavindadissanayake1999@gmail.com>
2025-07-04 16:03:29 +0530

    interfaces

--- chapter_5/src/app.ts
+++ chapter_5/src/app.ts
@@ -1,18 +1,39 @@
+interface IProduct {
+  code: string;
+}
+
 interface IPerson {
+  id: number;
   name: string;
   age: number;
 
   greet(name: string): void;
 }
 
-let user: IPerson;
+class User implements IPerson, IProduct {
+  id: number;
+  name: string = "John";
+  age: number = 30;
+  greet(name: string): void {
+    throw new Error("Method not implemented.");
+  }
 
-user = {
-  name: "John",
-  age: 30,
-  greet(name: string) {
-    console.log(`Hello, ${name}!`);
-  },
-};
+  code: string = "123";
 
-user.greet("Jane");
+  constructor(id: number) {
+    this.id = id;
+  }
+}
+
+// let user: IPerson;
+
+// user = {
+//   id: 1,
+//   name: "John",
+//   age: 30,
+//   greet(name: string) {
+//     console.log(`Hello, ${name}!`);
+//   },
+// };
+
+// user.greet("Jane");
